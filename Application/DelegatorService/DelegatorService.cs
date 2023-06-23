2e73996cbc4a26ac09755c1f78f165c01b42ccc2
Matthew Lewis <Mlewis@SINGULAR.CO.ZA>
2023-06-23 11:22:30 +0200

    Added the initial delegator service logic

---
+++ Application/DelegatorService/DelegatorService.cs
@@ -0,0 +1,37 @@
+﻿using Models.Interfaces.Services.Delegator;
+using Models.Interfaces.Services.ReadFromConfig;
+
+namespace Application.DelegatorService;
+
+/// <inheritdoc/>
+public class DelegatorService : IDelegatorService
+{
+  private readonly IReadFromConfigService readFromConfigService;
+  public DelegatorService(IReadFromConfigService readFromConfigService)
+  {
+    this.readFromConfigService = readFromConfigService;
+  }
+
+  /// <inheritdoc/>
+  public Task GetResponseAsync()
+  {
+    // Fetch Configured Settings from Config.json
+    var config = readFromConfigService.ReadFromConfig();
+
+    // Check if the specified branches/tags are present
+    // TODO:
+
+    // Fetch the latest changes for the specified branches/tags
+    // TODO:
+
+    // Generate the raw diffs for the specified branches/tags
+    // TODO:
+
+    // Cleanup & Group the diffs
+    // TODO:
+
+    // Save the cleaned diff to the configured path
+    // TODO:
+    return Task.CompletedTask;
+  }
+}
--- Console/Program.cs
+++ Console/Program.cs
@@ -1,7 +1,9 @@
-﻿using Application.ReadFromConfig;
+﻿using Application.DelegatorService;
+using Application.ReadFromConfig;
 using Domain;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Models.Interfaces.Services.Delegator;
 using Models.Interfaces.Services.JsonSerialiser;
 using Models.Interfaces.Services.ReadFromConfig;
 
@@ -12,7 +14,7 @@ public class Program
   /// <summary>
   /// The main console application entry point
   /// </summary>
-  public static void Main(string[] args)
+  public static async Task Main(string[] args)
   {
     // Create DI Container
     using var host = Host.CreateDefaultBuilder(args)
@@ -20,15 +22,14 @@ public class Program
       {
         // Register Services
         services.AddSingleton<IJsonSerialiser, JsonSerialiser>();
+        services.AddScoped<IDelegatorService, DelegatorService>();
         services.AddScoped<IReadFromConfigService, ReadFromConfigService>();
       })
       .Build();
 
-    // Resolve the dependencies
-    var service = host.Services.GetRequiredService<IReadFromConfigService>(); // TODO: calling this service for now (proof of concept)
-
-    // Use the service
-    service.ReadFromConfig();
+    // Call the delegator service
+    var service = host.Services.GetRequiredService<IDelegatorService>();
+    await service.GetResponseAsync();
 
     // Prevent automatic console closure
     Console.WriteLine("Press any key to exit...");
